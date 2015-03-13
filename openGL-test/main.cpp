acfc800e5d96912499921710b30fca813e9ee62b
Hannah Wolfe <hanawolfe@gmail.com>
2015-03-13 12:29:33 -0700

    updating plane class to handle planes as described

--- openGL-test/main.cpp
+++ openGL-test/main.cpp
@@ -129,161 +129,34 @@ class PRIMITIVE
     virtual void draw_p() = 0;
 };
 
-class CUBE:public PRIMITIVE{
-  private:
-    Vec3f P0;
-    Vec3f P1;
-    Vec3f P2;
-    Vec3f P3;
-    Vec3f P4;
-    Vec3f P5;
-    Vec3f P6;
-    Vec3f P7;
-  public:
-    CUBE(Vec3f p0, Vec3f p1, Vec3f p2, Vec3f p3, Vec3f p4, Vec3f p5, Vec3f p6, Vec3f p7, Vec3f color, Matrix4X4 transformation){
-      P0 = p0;
-      P1 = p1;
-      P2 = p2;
-      P3 = p3;
-      P4 = p4;
-      P5 = p5;
-      P6 = p6;
-      P7 = p7;
-      COLOR = color;
-      TRANSFORMATION = transformation;
-    }
-    void draw_p(){
-       glPushMatrix();
-        glRotatef(90.0, 0.0, 1.0, 0.0);
-        glTranslatef(-3.5,0.0,4.0);
-         glColor4f(0,1,0,1);
-        //define first triangle
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P0.x,P0.y,P0.z);
-          glVertex3f(P1.x,P1.y,P1.z);
-          glVertex3f(P2.x,P2.y,P2.z);
-        glEnd();
-        //define second triangle
-        glColor4f(1,0,0,1);
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P0.x,P0.y,P0.z);
-          glVertex3f(P2.x,P2.y,P2.z);
-          glVertex3f(P3.x,P3.y,P3.z);
-        glEnd();
-        glColor4f(0,1,0,1);
-        //define first triangle
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P3.x,P3.y,P3.z);
-          glVertex3f(P2.x,P2.y,P2.z);
-          glVertex3f(P4.x,P4.y,P4.z);
-        glEnd();
-        //define second triangle
-        glColor4f(1,0,0,1);
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P3.x,P3.y,P3.z);
-          glVertex3f(P4.x,P4.y,P4.z);
-          glVertex3f(P5.x,P5.y,P5.z);
-        glEnd();
-         glColor4f(0,1,0,1);
-        //define first triangle
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P5.x,P5.y,P5.z);
-          glVertex3f(P4.x,P4.y,P4.z);
-          glVertex3f(P6.x,P6.y,P6.z);
-        glEnd();
-        //define second triangle
-        glColor4f(1,0,0,1);
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P5.x,P5.y,P5.z);
-          glVertex3f(P6.x,P6.y,P6.z);
-          glVertex3f(P7.x,P7.y,P7.z);
-        glEnd();
-         glColor4f(0,1,0,1);
-        //define first triangle
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P7.x,P7.y,P7.z);
-          glVertex3f(P6.x,P6.y,P6.z);
-          glVertex3f(P1.x,P1.y,P1.z);
-        glEnd();
-        //define second triangle
-        glColor4f(1,0,0,1);
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P7.x,P7.y,P7.z);
-          glVertex3f(P1.x,P1.y,P1.z);
-          glVertex3f(P0.x,P0.y,P0.z);
-        glEnd();
-         glColor4f(0,1,0,1);
-        //define first triangle
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P0.x,P0.y,P0.z);
-          glVertex3f(P3.x,P3.y,P3.z);
-          glVertex3f(P5.x,P5.y,P5.z);
-        glEnd();
-        //define second triangle
-        glColor4f(1,0,0,1);
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P0.x,P0.y,P0.z);
-          glVertex3f(P5.x,P5.y,P5.z);
-          glVertex3f(P7.x,P7.y,P7.z);
-        glEnd();
-         glColor4f(0,1,0,1);
-        //define first triangle
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P1.x,P1.y,P1.z);
-          glVertex3f(P2.x,P2.y,P2.z);
-          glVertex3f(P4.x,P4.y,P4.z);
-        glEnd();
-        //define second triangle
-        glColor4f(1,0,0,1);
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P1.x,P2.y,P1.z);
-          glVertex3f(P4.x,P4.y,P4.z);
-          glVertex3f(P6.x,P6.y,P6.z);
-        glEnd();
-      glPopMatrix();
-      return;
-    } 
-    virtual ~CUBE() {}
-};
-
 class PLANE:public PRIMITIVE{
   private:
-    Vec3f P0;
-    Vec3f P1;
-    Vec3f P2;
-    Vec3f P3;
+    vector<Vec3f> POINTS;
+    Vec3f CENTROID;
+    Vec3f COLOR;
     int WALL;
     int CEILING;
     int FLOOR;
   public:
-    PLANE(Vec3f p0, Vec3f p1, Vec3f p2, Vec3f p3, Vec3f color, int wall, int ceiling, int floor, Matrix4X4 transformation){
-      P0 = p0;
-      P1 = p1;
-      P2 = p2;
-      P3 = p3;
+    PLANE(vector<Vec3f> points, Vec3f centroid, Vec3f color, int wall, int ceiling, int floor){
+      POINTS = points;
+      CENTROID = centroid;
       COLOR = color;
       WALL = wall;
       CEILING = ceiling;
       FLOOR = floor;
-      TRANSFORMATION = transformation;
     }
     void draw_p(){
       glPushMatrix();
         glRotatef(90.0, 0.0, 1.0, 0.0);
         glTranslatef(-3.5,0.0,4.0);
-        glColor4f(0,1,0,1);
-        //define first triangle
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P0.x,P0.y,P0.z);
-          glVertex3f(P1.x,P1.y,P1.z);
-          glVertex3f(P2.x,P2.y,P2.z);
-        glEnd();
-        //define second triangle
-        glColor4f(1,0,0,1);
-        glBegin(GL_TRIANGLES);
-          glVertex3f(P0.x,P0.y,P0.z);
-          glVertex3f(P2.x,P2.y,P2.z);
-          glVertex3f(P3.x,P3.y,P3.z);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLE_FAN);
+          glColor4f(0,1,0,1);
+          glVertex3f(CENTROID.x, CENTROID.y, CENTROID.z);
+ 	  for (int i = 0; i < POINTS.size(); i++){
+            glVertex3f(POINTS[i].x,POINTS[i].y,POINTS[i].z);
+          }
         glEnd();
       glPopMatrix();
       return;
@@ -341,26 +214,6 @@ class CYLINDER:public PRIMITIVE{
 };
 
 
-//draw square
-void drawSquare(float x1, float x2)
-{
-  glColor4f(0,1,0,1);
-  //define first triangle
-  glBegin(GL_TRIANGLES);
-    glVertex3f(x1,0.5f,0.0);
-    glVertex3f(x1,-0.5f,0.0);
-    glVertex3f(x2,-0.5f,0.0);
-  glEnd();
-  //define second triangle
-  glColor4f(1,0,0,1);
-  glBegin(GL_TRIANGLES);
-    glVertex3f(x1,0.5f,0.0);
-    glVertex3f(x2,0.5f,0.0);
-    glVertex3f(x2,-0.5f,0.0);
-  glEnd();
-}
-
-
 //////////////////////////////////////////////////////////////////////////////////
 // Draws to the OpenGL window
 //////////////////////////////////////////////////////////////////////////////////
@@ -376,8 +229,7 @@ void display()
   glPushMatrix();
   glRotatef(90.0, 0.0, 1.0, 0.0);
   glTranslatef(-3.5,0.0,4.0);
-//  glutSolidSphere(1.0,10.0,10.0);
-  drawSquare(-0.5,0.5);
+  glutSolidSphere(1.0,10.0,10.0);
   glPopMatrix();
   
   glutSwapBuffers();
@@ -520,25 +372,6 @@ void mouseButton(int button, int state, int x, int y)
 void mouseMotion(int x, int y)
 {
 }
-
-//////////////////////////////////////////////////////////////////////////////////
-// Read in a raw PPM file of the "P6" style.
-//
-// Input: "filename" is the name of the file you want to read in
-// Output: "pixels" will point to an array of pixel values
-//         "width" will be the width of the image
-//         "height" will be the height of the image
-//
-// The PPM file format is:
-//
-//   P6
-//   <image width> <image height>
-//   255
-//   <raw, 8-bit binary stream of RGB values>
-//
-// Open one in a text editor to see for yourself.
-//
-//////////////////////////////////////////////////////////////////////////////////
 
 //reshape when window resizes
 void reshape(int w, int h)
